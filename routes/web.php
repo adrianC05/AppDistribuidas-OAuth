3f87d13d1d95b009cf3c85511f6afe0e7093b57f
adrianC05 <aacarrion4@espe.edu.ec>
2025-03-07 22:21:15 -0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+use Laravel\Socialite\Facades\Socialite;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
+
+
+Route::get('/auth/redirect', [AuthController::class, 'redirect'])->name('auth.redirect');
+
+Route::get('/auth/callback', [AuthController::class, 'callback'])->name('auth.callback');
+
+require __DIR__.'/auth.php';
